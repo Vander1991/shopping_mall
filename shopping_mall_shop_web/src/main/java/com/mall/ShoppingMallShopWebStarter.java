177e702843bd49f309486d76ca149d62c3abae92
Vander1991 <398284823@qq.com>
2019-10-05 18:42:07 +0800

    功能增加：shop_web模块增加商家入驻（使用BCryptEncoder对用户注册的密码进行加密）；商家审核；商家登录

--- shopping_mall_shop_web/src/main/java/com/mall/ShoppingMallShopWebStarter.java
+++ shopping_mall_shop_web/src/main/java/com/mall/ShoppingMallShopWebStarter.java
@@ -11,8 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShoppingMallShopWebStarter {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         SpringApplication.run(ShoppingMallShopWebStarter.class);
+
     }
 
 }
